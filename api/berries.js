d97e423da9c0e1502ff53d109a5422ba3170405f
Jason Sawatzky <jasonmsawatzky@gmail.com>
2017-10-20 21:01:49 -0700

    Added support for local testing using serverless-offline. Refactored database related code to use serverless-dynamodb-client. Refactored database related code to use a Data Access Object.

--- api/berries.js
+++ api/berries.js
@@ -1,33 +1,26 @@
+/*
+** This is the berries handler. It handles requests to to '/berries',
+** and performs operations on Berries.
+**
+** HTTP and FaaS related semantics should be handled here.
+**
+** This handler injects depencies required for its functionality.
+** Ex. dynamoDb
+*/
+
+'use strict';
+
 const AWS = require('aws-sdk');
 const uuid = require('uuid');
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const BerryDAO = require('./dao/BerryDAO.js');
+const dynamoDb = require('serverless-dynamodb-client');
+var berryDAO = new BerryDAO(dynamoDb.doc);
 
 module.exports.create = (event, context, callback) => {
-  console.log('body' + event.body);
-  const timestamp = new Date().getTime();
-  const data = JSON.parse(event.body);
-  if (typeof data.notes !== 'string') {
-    console.error('Validation Failed');
-    callback(null, {
-      statusCode: 400,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Couldn\'t create the berry. Invalid berry',
-    });
-    return;
-  }
-
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: {
-      id: uuid.v1(),
-      notes: data.notes,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    },
-  };
-
-  // write the berry to the database
-  dynamoDb.put(params, (error) => {
+  const body = JSON.parse(event.body);
+  var berry = {};
+  berry.notes = body.notes;
+  berryDAO.save(berry, (error, result) => {
     // handle potential errors
     if (error) {
       console.error(error);
@@ -36,25 +29,22 @@ module.exports.create = (event, context, callback) => {
         headers: { 'Content-Type': 'text/plain' },
         body: 'Couldn\'t create the berry. Database error.',
       });
-      return;
     }
-
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
+    else {
+      // create a response
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    }
   });
-};
+}
 
-const params = {
-  TableName: process.env.DYNAMODB_TABLE,
-};
+
 
 module.exports.list = (event, context, callback) => {
-  // fetch all berries from the database
-  dynamoDb.scan(params, (error, result) => {
+  berryDAO.list((error, result) => {
     // handle potential errors
     if (error) {
       console.error(error);
@@ -63,19 +53,19 @@ module.exports.list = (event, context, callback) => {
         headers: { 'Content-Type': 'text/plain' },
         body: 'Couldn\'t fetch the berries.',
       });
-      return;
     }
-
-    // create a response
-    const response = {
-      statusCode: 200,
-      headers: {
-          "Content-Type": "application/javascript",
-          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+    else {
+      // create a response
+      const response = {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/javascript",
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
         },
-      body: JSON.stringify(result.Items),
-    };
-    callback(null, response);
+        body: JSON.stringify(result.Items),
+      };
+      callback(null, response);
+    }
   });
 };
---
+++ api/dao/BerryDAO.js
@@ -0,0 +1,48 @@
+/*
+** This is the Berry Data Access Object, which provides access to the Berries
+** table.
+**
+** This controls access to the Berries table, based on business rules.
+** Ex. A new Berry could be rejected because it is missing a required property.
+** This class can pass error messages upstream in the callback, but HTTP
+** semantics (HTTP Error Codes) should be handled in the berries.js handler.
+*/
+
+'use strict';
+
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+class BerryDAO {
+  constructor(db) {
+    this.db = db;
+  }
+
+  save(berry, callback) {
+
+    const timestamp = new Date().getTime();
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1(),
+        notes: berry.notes,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    };
+
+    // write the berry to the database
+    this.db.put(params, callback);
+  }
+
+  list(callback) {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+    };
+
+    // fetch all berries from the database
+    this.db.scan(params, callback);
+  }
+}
+
+module.exports = BerryDAO;
