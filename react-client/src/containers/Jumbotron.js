af37684284251ced87479e023e17ef08c79d9e1f
Derek Lance <dlance@calpoly.edu>
2017-11-26 22:57:08 -0800

    Update Cognito callback url to reflect server port changes

--- react-client/src/containers/Jumbotron.js
+++ react-client/src/containers/Jumbotron.js
@@ -8,7 +8,7 @@ export default class Jumbotron extends Component {
               <div className="container">
                 <h1 className="display-3">Kotoka</h1>
                 <p>Strawberry Harvest Solutions</p>
-                <p><a className="btn btn-primary btn-lg" href="https://github.com/CPSECapstone/kotoka-webapp-api" role="button">Github</a></p>
+                <p><a className="btn btn-primary btn-lg" href="https://github.com/CPSECapstone/kotoka-webapp-api" role="button">GitHub</a></p>
               </div>
             </div>
         )
