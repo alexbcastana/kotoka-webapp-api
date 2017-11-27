eb7153e1c9e542a57ea4f677b1d9ee7644cfcad1
Derek Lance <dlance@calpoly.edu>
2017-11-26 20:53:23 -0800

    Fix spelling errors

--- react-client/src/containers/Home.js
+++ react-client/src/containers/Home.js
@@ -54,7 +54,7 @@ export default class Home extends Component {
         <div className="row">
           <div className="col-md-4">
             <h2>Automation, enhanced by human intellect</h2>
-            <p>Kotoka leverages croud sourced intellect from people around the world to identify and asses berries in the field.</p>
+            <p>Kotoka leverages crowdsourced intellect from people around the world to identify and assess berries in the field.</p>
             <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div>
           <div className="col-md-4">
@@ -77,11 +77,7 @@ export default class Home extends Component {
       </div>
 
 
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-      <script src="aws-cognito-sdk.min.js"></script>
-      <script src="aws-cognito-identity.min.js"></script>
+
       </html>
     );
   }
