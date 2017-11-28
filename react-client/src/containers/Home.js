5fa11d776a40ccc5b4ba413582a732863d1b975b
Derek Lance <dlance@calpoly.edu>
2017-11-27 21:59:43 -0800

    Fix footer styling

--- react-client/src/containers/Home.js
+++ react-client/src/containers/Home.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Navbar from './Navbar';
 import Jumbotron from './Jumbotron';
 import Container from './Container';
-import Footer from './Footer';
 
 const Home = (props) => (
     <div>
         <Navbar />
         <Jumbotron />
         <Container />
-        <Footer />
     </div>
 );
 
