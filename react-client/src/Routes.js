2e6fa0c92b048378c6dc1c471bf312fc500ba695
Derek Lance <dlance@calpoly.edu>
2017-11-27 21:50:52 -0800

    Refactor homepage code

--- react-client/src/Routes.js
+++ react-client/src/Routes.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
+import Home from './containers/Home';
+
+const Routes = (props) => (
+    <Route path='/' exact component={ Home } />
+);
+
+export default Routes;
---
+++ react-client/src/containers/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
+import Container from './Container';
+import Footer from './Footer';
+
+const Home = (props) => (
+    <div>
+        <Navbar />
+        <Jumbotron />
+        <Container />
+        <Footer />
+    </div>
+);
+
+export default Home;
