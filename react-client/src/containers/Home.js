1c7a808cd59cb7d79db1f2c7f62711217cf224f5
Derek Lance <dlance@calpoly.edu>
2017-11-30 11:56:16 -0800

    Refactor code

--- react-client/src/containers/Home.js
+++ react-client/src/containers/Home.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Jumbotron from './Jumbotron';
 import Container from './Container';
 
-const Home = (props) => (
-    <div>
-        <Navbar />
-        <Jumbotron />
-        <Container />
-    </div>
-);
+class Home extends Component {
+    displayUsername() {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                {this.displayUsername()}
+                <Navbar />
+                <Jumbotron />
+                <Container />
+            </div>
+        );
+    }
+}
 
 export default Home;
