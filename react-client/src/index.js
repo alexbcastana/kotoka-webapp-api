54dca6741e47db2b794af03bdd1e6e650ae5d417
Alexander Chamberlain <alxchamberlain@gmail.com>
2018-01-23 21:44:35 -0800

    Setup Redux and add base User reducer

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from "./App";
+import reducers from './reducers/index.js';
 import "./styles/index.css";
 
-ReactDOM.render(
-   <Router>
-      <App />
-   </Router>,
-   document.getElementById("root")
+const store = createStore(reducers);
+const target = document.getElementById('root')
+
+const router = (
+   <Provider store={store}>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </Provider>
+)
+
+render(
+   router,
+   target
 );
